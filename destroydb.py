3cce90dd9713c0977dd791763b2d8484b9591433
Quinta13 <878500@stud.unive.it>
2021-05-21 16:38:11 +0200

    destroydb
    script to clean up database

---
+++ destroydb.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+# engine = create_engine('sqlite:///database.db', echo=True)
+engine = create_engine('postgresql://postgres:1sebaQuinta@localhost:5432/Gym', echo=True)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.metadata.drop_all(engine)  
