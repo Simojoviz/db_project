0d0db3c316320e1d0eda1a6530ba784f5d9eb2d2
Quinta13 <878500@stud.unive.it>
2021-05-30 11:44:35 +0200

    move db with relationship to a separeted file

--- destroydb.py
+++ destroydb.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import sessionmaker
-import datetime
+
 
 # engine = create_engine('sqlite:///database.db', echo=True)
 engine = create_engine('postgresql://postgres:1sebaQuinta@localhost:5432/Gym', echo=False)
