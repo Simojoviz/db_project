8ce471ad3f6849dbe5bcc9a470a001d6145a2a0a
Andrea Rosa <882014@stud.unive.it>
2021-08-27 11:50:30 +0200

    add main queries

--- pgAdmin_queries.sql
+++ pgAdmin_queries.sql
@@ -21,3 +21,36 @@ SELECT u.fullname AS name, c.name AS course_name, u2.fullname AS trainer
 FROM users u JOIN course_signs_up csu ON u.id = csu.user_id
 	JOIN courses c ON csu.course_id = c.id
 	JOIN users u2 ON c.instructor_id = u2.id
+
+-- User remaining Prenotations --
+-- in prenotations page --
+SELECT s.date, s.starting, s.ending, r.name AS room_name
+FROM users u JOIN prenotations p ON u.id = p.user_id 
+		     JOIN shifts s ON  s.id = p.shift_id
+		     JOIN rooms r ON s.room_id = r.id
+WHERE u.fullname = 'Name Surname' AND ((s.date = 'YYYY-MM-DD' AND s.ending >= 'hh:mm:ss') OR s.date > 'YYYY-MM-DD')
+-- current date and current time
+
+-- User Current Courses sign ups --
+SELECT c.name, c.starting, c.ending
+FROM users u JOIN course_signs_up cs ON u.id = cs.user_id
+				JOIN courses c ON c.id = cs.course_id
+WHERE u.fullname = 'Name Surname' AND c.ending >= 'YYYY-MM-DD'
+
+-- Course shifts --
+SELECT s.date, s.starting, s.ending, r.name as room_name
+FROM courses c JOIN shifts s ON c.id = s.course_id 
+				JOIN rooms r ON r.id = s.room_id
+WHERE c.name = 'Course Name' AND s.date >= 'YYYY-MM-DD'
+
+-- Course users --
+SELECT u.fullname
+FROM users u JOIN course_signs_up cs ON u.id = cs.user_id 
+			 JOIN courses c ON c.id = cs.course_id
+WHERE c.name='Course Name'
+
+-- Shift users --
+SELECT u.fullname
+FROM shifts s JOIN prenotations p ON s.id = p.shift_id
+			  JOIN users u ON u.id = p.user_id
+WHERE s.date = 'YYYY-MM-DD' AND s.starting = 'hh:mm:ss'
