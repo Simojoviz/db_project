cfe2892fcf1881bf1a198e1f20a14810acc2fa79
Andrea Rosa <882014@stud.unive.it>
2021-08-29 11:50:32 +0200

    delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-
-from automap import *
-
-# engine = create_engine('sqlite:///database.db', echo=True)
-#engine = create_engine('postgresql://postgres:1sebaQuinta@localhost:5432/Gym', echo=False)
-engine = create_engine('postgresql://postgres:Simone01@localhost:5432/Gym', echo=False)
-
